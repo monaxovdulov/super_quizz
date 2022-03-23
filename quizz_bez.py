bec7cf7893ea8dc85e67276eee2bc763a82f26f9
Anton Betsun <bee.tzung.jr@gmail.com>
2022-03-23 15:21:20 +0200

    Add test console UI

--- quizz_bez.py
+++ quizz_bez.py
@@ -1,19 +1,53 @@
 import json
 
-from game import Game
+from game import Game, FLAG_GAME_STARTED
 from question import Question
 
 
-def main():
+def test_console_run():
     # read json file
     with open('quizz.json') as json_file:
         data = json.load(json_file)
+
+    # parse questions
     questions = []
     for q in data:
         questions.append(Question(q['text'], q['correct'], q['category'], q['score'], q['options']))
+
+    # create game
     game = Game(questions)
+
+    # start game
     game.start()
+
+    # game loop
+    while game.status == FLAG_GAME_STARTED:
+        question = game.get_current_question()
+
+        # print question and answer options
+        print(question.text)
+        counter = 1
+        for option in question.options:
+            print(f"{counter}: {option}")
+
+        # get user answer
+        answer = int(input("Select answer"))
+
+        # check answer
+        is_correct = game.answer(answer)
+        if is_correct:
+            print(f"Correct! score: {game.score}")
+        else:
+            print(f"Wrong! correct answer was: {question.get_correct_answer()}")
+
+    # game is finished, show final score
+    print(f"Total score: {game.score}")
+
+
+def main():
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    test_console_run()
